c674710f437d1ac7365c1e8bd06c2047ec233233
x7Akimm <titomahdi48@gmail.com>
2024-10-21 23:08:59 +0100

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,41 @@
+tsParticles
+.load({
+    id: "tsparticles",
+    url: "../tsparticles.config.json",
+})
+.then(container => {
+    console.log("callback - tsparticles config loaded");
+    
+})
+.catch(error => {
+    console.error(error);
+});
+tsParticles
+.load({
+    id: "tsparticles1",
+    url: "../tsparticles.config.json",
+})
+.then(container => {
+    console.log("callback - tsparticles config loaded");
+    
+})
+.catch(error => {
+    console.error(error);
+});
+
+
+
+// tsParticles
+// .load({
+//     id: "tsparticles1",
+//     url: "../tsparticles.config.json",
+// })
+// .then(container => {
+//     console.log("callback - tsparticles config loaded");
+    
+// })
+// .catch(error => {
+//     console.error(error);
+// });
+
+
